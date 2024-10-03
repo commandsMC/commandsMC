04b93ed46d8cf18d6a3c000ef8a8c0232e47fd5e
Francisco Griman <grihardware@gmail.com>
2024-10-03 17:49:19 -0400

    Refactor stop_event_bot and add stop_all_events_bot functions

--- minechat/_commands.py
+++ minechat/_commands.py
@@ -1,9 +1,13 @@
-from .utils.events import stop_event
+from .utils.events import stop_event, stop_all_events
 
 def stop_event_bot(bot, message):
     """
     Stop an event by name.
     """
+    if bot.admin and not message.user == bot.admin:
+        bot.chat('You are not the admin.')
+        return
+    
     if not len(message.message.split()) == 2:
         bot.chat('Invalid command. Example !stop <NAME FUNCTION>')
         return
@@ -12,4 +16,16 @@ def stop_event_bot(bot, message):
         bot.chat('Event stopped.')
     else:
         bot.chat('Event not found.')
+    return
+
+def stop_all_events_bot(bot, message):
+    """
+    Stop all events.
+    """
+    if bot.admin and not message.user == bot.admin:
+        bot.chat('You are not the admin.')
+        return
+    
+    stop_all_events()
+    bot.chat('All events stopped.')
     return
